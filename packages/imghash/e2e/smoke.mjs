c16816b52ba44e81ce8720c385b9d913be166f0d
Paweł Maciejewski <1276139+pwlmc@users.noreply.github.com>
2025-08-02 19:33:02 +0200

    chore: move imghash to a npm workspace package (#266)
    Co-authored-by: Paweł Maciejewski <1276139+pwlmaciejewski@users.noreply.github.com>

--- packages/imghash/e2e/smoke.mjs
+++ packages/imghash/e2e/smoke.mjs
@@ -5,6 +5,6 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const hash = await imghash.hash(__dirname + "/../assets/absolut2");
+const hash = await imghash.hash(__dirname + "/../../../assets/absolut2");
 
 console.log(hash); // e781c3c3c3c3819f
