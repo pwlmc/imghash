b80dabc02f17cf7c254f52339a206a0ac7008084
Paweł Maciejewski <1276139+pwlmaciejewski@users.noreply.github.com>
2025-08-02 19:22:44 +0200

    chore: move imghash to a npm workspace package

--- packages/imghash/e2e/smoke.cjs
+++ packages/imghash/e2e/smoke.cjs
@@ -1,5 +1,5 @@
 const imghash = require("../dist/index.js");
 
-imghash.hash(__dirname + "/../assets/absolut2").then((hash) => {
+imghash.hash(__dirname + "/../../../assets/absolut2").then((hash) => {
   console.log(hash); // e781c3c3c3c3819f
 });
