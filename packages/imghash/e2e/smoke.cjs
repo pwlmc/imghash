d5be49d4c87ed655d0eb13f86b318ae587199112
Paweł Maciejewski <1276139+pwlmc@users.noreply.github.com>
2026-04-08 16:08:48 +0200

    chore: update tsdown (#355)

--- packages/imghash/e2e/smoke.cjs
+++ packages/imghash/e2e/smoke.cjs
@@ -1,4 +1,4 @@
-const imghash = require("../dist/index.js");
+const imghash = require("../dist/index.cjs");
 
 imghash.hash(__dirname + "/../../../assets/absolut2").then((hash) => {
   console.log(hash); // e781c3c3c3c3819f
